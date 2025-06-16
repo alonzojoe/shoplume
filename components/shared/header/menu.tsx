bda7f5914e545ee499ed0c74d38bc44a4edb70bd
alonzojoe <alonzojoenell2k15@gmail.com>
2025-06-16 16:09:37 +0800

    add responsiveness in the header nav items using shadcn sheet component

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,7 +1,14 @@
 import { Button } from "@/components/ui/button";
 import ModeToggle from "./mode-toggle";
 import Link from "next/link";
-import { ShoppingCart, UserIcon } from "lucide-react";
+import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetTitle,
+  SheetDescription,
+} from "@/components/ui/sheet";
 
 const Menu = () => {
   return (
@@ -19,6 +26,29 @@ const Menu = () => {
             Sign In
           </Link>
         </Button>
+      </nav>
+      <nav className="md:hidden">
+        <Sheet>
+          <SheetTrigger className="align-middle">
+            <EllipsisVertical />
+          </SheetTrigger>
+          <SheetContent className="flex flex-col items-start p-4">
+            <SheetTitle>Menu</SheetTitle>
+            <ModeToggle />
+            <Button asChild variant={"ghost"}>
+              <Link href="/cart">
+                <ShoppingCart /> Cart
+              </Link>
+            </Button>
+            <Button asChild>
+              <Link href="/sign-in">
+                {/* <UserIcon />  */}
+                Sign In
+              </Link>
+            </Button>
+            <SheetDescription></SheetDescription>
+          </SheetContent>
+        </Sheet>
       </nav>
     </div>
   );

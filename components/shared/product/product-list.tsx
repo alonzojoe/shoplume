ad30742d5d4d27e13d052f3171f605c200682c37
alonzojoe <alonzojoenell2k15@gmail.com>
2025-09-11 21:21:48 +0800

    display products in product-list. add new liimt prop and slice the data passed in product-list

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,7 +1,28 @@
-const ProductList = ({ data, title }: { data: any; title?: string }) => {
+const ProductList = ({
+  data,
+  title,
+  limit,
+}: {
+  data: any;
+  title?: string;
+  limit: number;
+}) => {
+  const limitedData = limit ? data.slice(0, limit) : data;
+
   return (
     <div className="my-10">
-      <h2 className="h2-bold mb-4 text-white">{title}</h2>
+      <h2 className="h2-bold mb-5">{title}</h2>
+      {limitedData.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {limitedData.map((product: any, index: number) => (
+            <div key={`${product.name}-${index}`}>{product.name}</div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No Products Found</p>
+        </div>
+      )}
     </div>
   );
 };

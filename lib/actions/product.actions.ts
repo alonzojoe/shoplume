98842272ca8c66686063dc30e7cc241d4f0c6220
alonzojoe <alonzojoenell2k15@gmail.com>
2025-09-12 14:55:39 +0800

    store PRODUCT_LIMIT in .env, implement it in getLatestProducts config, install new library zod

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,13 +1,15 @@
 
 'use server'
 import { prisma } from "@/lib/prisma"
+import { convertToPlainObject } from "@/lib/utils"
+import { LATEST_PRODUCTS_LIMIT } from "@/lib/constants"
 
 export const getLatestProducts = async () => {
 
     const data = await prisma.product.findMany({
-        take: 4,
+        take: LATEST_PRODUCTS_LIMIT,
         orderBy: { createdAt: 'desc' }
     })
 
-    return JSON.parse(JSON.stringify(data))
+    return convertToPlainObject(data)
 }

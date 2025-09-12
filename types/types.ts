41c4628eaecaf022f7d63faf0a4dd986cbc6dfb2
alonzojoe <alonzojoenell2k15@gmail.com>
2025-09-12 16:10:19 +0800

    create new schema for product insert implement re-use validation schema in types using z.infer of zod

--- types/types.ts
+++ types/types.ts
@@ -1,14 +1,24 @@
-export type Product = {
-    name: string;
-    slug: string;
-    category: string;
-    description: string;
-    images: string[];
-    price: number;
-    brand: string;
+
+import { z } from "zod"
+import { insertProductSchema } from "@/lib/validateors";
+
+export type Product = z.infer<typeof insertProductSchema> & {
+    id: string,
     rating: number;
     numReviews: number;
-    stock: number;
-    isFeature: boolean;
-    banner: string | null;
+    createdAt: Date;
 }
+// {
+//     name: string;
+//     slug: string;
+//     category: string;
+//     description: string;
+//     images: string[];
+//     price: number;
+//     brand: string;
+//     rating: number;
+//     numReviews: number;
+//     stock: number;
+//     isFeature: boolean;
+//     banner: string | null;
+// }
